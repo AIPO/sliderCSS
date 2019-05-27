2a27544d25d74259660e81ad510131fd34fde3bd
povilas_kiro <povilas@kiro.tech>
2019-05-27 20:30:07 +0300

    functions to make slide changes

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+const nextSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    //Check for next slide
+    if (current.nextElementSibling) {
+        current.nextElementSibling.classList.add('current');
+    } else {
+        slides[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+const prevSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    //Check for prev slide
+    if (current.previousElementSibling) {
+        current.previuosElementSibling.classList.add('current');
+    } else {
+        //add current to last
+        slides[slides.length -1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}

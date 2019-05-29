a55271b4f8230b70f1b70bec62fa8e68eedf0b40
povilas_kiro <povilas@kiro.tech>
2019-05-29 21:13:34 +0300

    added responsive for content and autoslide effect

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
-const auto = false;
+const auto = true;
 const intervalTime = 5000;
 let slideInterval;
 const nextSlide = () => {
@@ -23,13 +23,26 @@ const prevSlide = () => {
         current.previousElementSibling.classList.add('current');
     } else {
         //add current to last
-        slides[slides.length-1].classList.add('current');
+        slides[slides.length - 1].classList.add('current');
     }
     setTimeout(() => current.classList.remove('current'));
 }
 next.addEventListener('click', e => {
     nextSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
 });
 prev.addEventListener('click', e => {
     prevSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
 });
+//Auto slide
+if (auto) {
+    //run next slide
+    slideInterval = setInterval(nextSlide, intervalTime);
+}
